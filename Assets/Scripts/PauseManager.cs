024a5f5a43289dae5afb96e8d8addecd3b39176d
vituhaa <vbuzina72@gmail.com>
2024-11-13 14:35:33 +0300

    the extra scenes were deleted, the pause was made through the escape button

---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    private bool isPaused = false;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                ResumeGame();
+            else
+                PauseGame();
+        }
+    }
+
+    void PauseGame()
+    {
+        Time.timeScale = 0f;
+        isPaused = true;
+
+        // Pause all audio
+        AudioListener.pause = true;
+    }
+
+    void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        isPaused = false;
+
+        // Resume all audio
+        AudioListener.pause = false;
+    }
+}
